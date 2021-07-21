f6a9797b48e3c25d1c3b2f1956310279193b7352
Afonso H Sabino <afonsohs@gmail.com>
2021-07-21 14:24:16 -0300

    Otimização da UI

---
+++ Devote/View/BackgroundImageView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundImageView.swift
+//  Devote
+//
+//  Created by Afonso Sabino on 21/07/21.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImageView()
+    }
+}
--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -30,47 +30,60 @@ struct ContentView: View {
     var body: some View {
         // Embed "List" em um novo container (NavigationView)
         NavigationView {
-            VStack {
-                VStack(alignment: .center, spacing: 16, content: {
-                    TextField("New Task", text: $task)
+            ZStack {
+                VStack {
+                    VStack(alignment: .center, spacing: 16, content: {
+                        TextField("New Task", text: $task)
+                            .padding()
+                            .background(
+                                Color(UIColor.systemGray6)
+                            )
+                            .cornerRadius(10)
+                        
+                        Button(action: {
+                            addItem()
+                        }, label: {
+                            Spacer()
+                            Text("SAVE")
+                            Spacer()
+                        }) //: BUTTON
+                        .disabled(isButtonDisable)
                         .padding()
-                        .background(
-                            Color(UIColor.systemGray6)
-                        )
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        // Condicional para status do botão
+                        .background(isButtonDisable ? Color.gray : Color.pink)
                         .cornerRadius(10)
-                    
-                    Button(action: {
-                        addItem()
-                    }, label: {
-                        Spacer()
-                        Text("SAVE")
-                        Spacer()
-                    }) //: BUTTON
-                    .disabled(isButtonDisable)
+                    }) //: VSTACK
                     .padding()
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    // Condicional para status do botão
-                    .background(isButtonDisable ? Color.gray : Color.pink)
-                    .cornerRadius(10)
-                }) //: VSTACK
-                .padding()
-                
-                List {
-                    ForEach(items) { item in
-                        VStack(alignment: .leading) {
-                            Text(item.task ?? "")
-                                .font(.headline)
-                                .fontWeight(.bold)
-                            
-                            Text("\(item.timestamp!, formatter: itemFormatter)")
-                                .font(.footnote)
-                                .foregroundColor(.gray)
-                        } //: LIST ITEM
-                    }
-                    .onDelete(perform: deleteItems)
-                } //: LIST
-            } //: VSTACK
+                    
+                    List {
+                        ForEach(items) { item in
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                
+                                Text("\(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            } //: LIST ITEM
+                        }
+                        .onDelete(perform: deleteItems)
+                    } //: LIST
+                    //Apresentar a lista em bloco com bordas arrendondas
+                    .listStyle(InsetGroupedListStyle())
+                    //Acrescenta um sombreamento na lista
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    //Ajuste do frame da listagem para visualização em iPAD
+                    .padding(.vertical, 0)
+                    .frame(maxWidth: 640)
+                } //: VSTACK
+            } //: ZSTACK
+            //Remove o background da tableView
+            .onAppear(perform: {
+                UITableView.appearance().backgroundColor = UIColor.clear
+            })
             .navigationBarTitle("Daily Tasks", displayMode: .large)
             .toolbar {
                 #if os(iOS)
@@ -78,15 +91,23 @@ struct ContentView: View {
                     EditButton()
                 }
                 #endif
-//
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    Button(action: addItem) {
-//                        Label("Add Item", systemImage: "plus")
-//                    }
-//                }
+    //
+    //                ToolbarItem(placement: .navigationBarTrailing) {
+    //                    Button(action: addItem) {
+    //                        Label("Add Item", systemImage: "plus")
+    //                    }
+    //                }
                 
             } //: TOOLBAR
+            .background(
+                BackgroundImageView()
+            )
+            .background(
+                backgroundGradient.ignoresSafeArea(.all)
+            )
         } //: NAVIGATION
+        //Apresentar uma única coluna mesmo em visualizações no iPad
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 
     // MARK: - FUNCTIONS
